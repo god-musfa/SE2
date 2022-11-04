d538a7024a991f863efa7e873d4ae61c2dd3f1de
Dennis Heil <dennis.heil@smail.inf.h-brs.de>
2022-11-04 15:28:47 +0100

    define SoftwareeAvatar component via interface

---
+++ src/main/java/org/hbrs/se2/project/hellocar/components/SoftwareeAvatarIf.java
@@ -0,0 +1,21 @@
+package org.hbrs.se2.project.hellocar.components;
+
+/**
+ * Interface for Custom Avatar Component
+ *
+ * This component displays an avatar image with optional label or upload option.
+ */
+
+public interface SoftwareeAvatarIf {
+    // Sets the image for the avatar
+    public void setImage(String imagePath);
+
+    // Sets the optional label beneath the avatar (right side)
+    public void setLabel(String labelText);
+
+    // Set the cubic size of the avatar image to a given value
+    public void setSize(String sizeValue);
+
+    // Enables or disables the option to change the profile picture
+    public void setChangeOption(boolean state);
+}
