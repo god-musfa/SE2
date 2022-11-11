c411eae1a2ba6c8cd0d1b4f2325ba6464e111605
Michael Mathias Lischka <michael.lischka@smail.inf.h-brs.de>
2022-11-11 15:15:31 +0100

    RegisterStudentView and RegisterView Component setup

---
+++ src/main/java/org/hbrs/se2/project/softwaree/views/RegisterStudentView.java
@@ -0,0 +1,61 @@
+package org.hbrs.se2.project.softwaree.views;
+import com.vaadin.*;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.textfield.TextField;
+
+public class RegisterStudentView {
+  //Anrede - TextfeldS
+  Select anr = new Select<>();
+  public void initializeANR() {
+    anr.setLabel("Anrede");
+    anr.setItems("Herr","Frau");
+    anr.setRequired(true);
+  }
+  //Vorname - Textfeld
+  TextField vn = new TextField("Vorname");
+  public void initializeVN() {
+    vn.setMaxLength(20);
+    vn.setRequired(true);
+  }
+  // Nachname - Textfeld
+  TextField nn = new TextField("Nachname");
+  public void initializeNN() {
+    nn.setMaxLength(20);
+    nn.setRequired(true);
+  }
+  // Straße, Hausnummer - Textfeld
+  TextField sthn = new TextField("Straße, Hausnummer");
+  public void initializeSTHN() {
+    sthn.setMaxLength(20);
+    sthn.setRequired(true);
+    sthn.setPattern("([a-zA-Z]|[ß]|[ ])+, [0-9]+\n");
+  }
+  // PLZ - Textfeld
+  TextField plz = new TextField("PLZ");
+  public void initializePLZ() {
+    plz.setMaxLength(20);
+    plz.setRequired(true);
+    plz.setPattern("\d\d\d\d\d");
+  }
+  // Abschluss - Textfeld
+  Select abs = new Select<>();
+  public void initializeABS() {
+    abs.setLabel("Abschluss");
+    abs.setItems("Bachelor","Master","Doktor");
+    abs.setRequired(true);
+  }
+  // Universität - Textfeld
+  TextField uni = new TextField("Universität");
+  public void initializeUNI() {
+    uni.setMaxLength(20);
+    uni.setRequired(true);
+  }
+  //Registrieren - Button
+  Button registerButton = new Button("Registrieren");
+  public void initializeButton() {
+    registerButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+  }
+}
+
---
+++ src/main/java/org/hbrs/se2/project/softwaree/views/RegisterView.java
@@ -0,0 +1,77 @@
+package org.hbrs.se2.project.softwaree.views;
+import com.vaadin.*;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.button.Button;
+import javax.validation.constraints.Email;
+
+public class RegisterView extends VerticalLayout {
+  //Benutzername - Textfeld
+  TextField un = new TextField();
+  public void initializeUN() {
+    un.setMaxLength(20);
+    un.setRequired(true);
+  }
+
+  //E-Mail - Textfeld
+  EmailField em = new EmailField("E-Mail");
+  public void initializeEM() {
+    em.setMaxLength(32);
+    em.setRequired(true);
+  }
+
+  //Passwort, mind. 12 Zeichen - Passwort-Textfeld
+  PasswordField pw = new PasswordField("Passwort");
+  public void initializePW() {
+    pw.setRevealButtonVisible(false);
+    pw.setMaxLength(32);
+    pw.setMinLength(12);
+    pw.setHelperText("Das Passwort muss mindestens 12 Zeichen haben.");
+    pw.setErrorMessage("Kein valides Passwort");
+    pw.setRequired(true);
+  }
+
+  //Passwort wiederholen - Passwort Textfeld
+  PasswordField pw2 = new PasswordField("Passwort wiederholen");
+  public void initializePW2() {
+    pw2.setRevealButtonVisible(false);
+    pw2.setMaxLength(32);
+    pw2.setMinLength(12);
+    pw2.setRequired(true);
+  }
+
+  //Ich stimme der AGB und der Datenschutzbestimmung zu: - Checkmark
+  Checkbox agb = new Checkbox("Ich stimme der AGB und der Datenschutzbestimmung zu.");
+
+
+  //Registrieren - Button
+  Button registerButton = new Button("Registrieren");
+  public void initializeButton() {
+    registerButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+  }
+
+  public RegisterView() {
+    addClassName("RegisterView");
+
+    setId("registerView");
+    initializeEM();
+    initializeUN();
+    initializePW();
+    initializePW2();
+    initializeButton();
+
+    VerticalLayout layout = new VerticalLayout();
+    layout.add(em,un,pw,pw2,agb,registerButton);
+    add(layout);
+
+  }
+
+
+}
