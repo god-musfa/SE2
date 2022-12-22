412ad3ac07f63516aab2e436aa769b06b722ce1f
Mervan M. Hella <merfan.halah@smail.inf.h-brs.de>
2022-12-22 18:04:23 +0100

    Job Detail Control- Test

---
+++ src/test/java/org/hbrs/se2/project/softwaree/test/control/JobDetailControlTest.java
@@ -0,0 +1,57 @@
+package org.hbrs.se2.project.softwaree.test.control;
+
+import org.hbrs.se2.project.softwaree.control.JobDetailControl;
+import org.hbrs.se2.project.softwaree.control.JobOfferControl;
+import org.hbrs.se2.project.softwaree.dtos.JobDTO;
+import org.hbrs.se2.project.softwaree.repository.BlacklistRepository;
+import org.hbrs.se2.project.softwaree.repository.JobRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class JobDetailControlTest {
+
+    @Autowired
+    JobRepository jobRepository;
+
+    @Autowired
+    BlacklistRepository blacklistRepository;
+
+
+    @Autowired
+    JobDetailControl jobDetailControl ;
+
+
+    @Test
+    void getJob() {
+
+        JobDTO jobDTO = new JobDTO(2, "Test", "test" ,"test");
+        assertEquals(jobDTO.getId() , jobDetailControl.getJob(2).getId());
+        assertEquals(jobDTO.getDescription() , jobDetailControl.getJob(2).getDescription());
+        assertEquals(jobDTO.getLocation() , jobDetailControl.getJob(2).getLocation());
+        assertEquals(jobDTO.getTitle() , jobDetailControl.getJob(2).getTitle());
+
+
+    }
+
+    @Test
+    void validateJobID() {
+        assertTrue(jobDetailControl.validateJobID(2));
+        assertFalse(jobDetailControl.validateJobID(9999));
+    }
+
+    @Test
+    void validateJobViews() {
+
+        assertEquals(2, jobDetailControl.validateJobViews(2));
+        assertEquals(0, jobDetailControl.validateJobViews(null));
+    }
+
+   /* @Test
+    void addBlacklist() {
+
+    }*/
+}
