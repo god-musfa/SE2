16e7acd49b170d5258beeeac22a4c372b84c0296
Mervan M. Hella <merfan.halah@smail.inf.h-brs.de>
2022-12-26 18:01:49 +0100

    neue tests für JobOfferControl

--- src/test/java/org/hbrs/se2/project/softwaree/test/control/JobOfferControlTest.java
+++ src/test/java/org/hbrs/se2/project/softwaree/test/control/JobOfferControlTest.java
@@ -1,26 +1,22 @@
 package org.hbrs.se2.project.softwaree.test.control;
 
 import org.hbrs.se2.project.softwaree.control.JobOfferControl;
-import org.hbrs.se2.project.softwaree.control.factories.SkillFactory;
-import org.hbrs.se2.project.softwaree.dtos.BenefitDTO;
-import org.hbrs.se2.project.softwaree.dtos.JobDTO;
-import org.hbrs.se2.project.softwaree.dtos.RequirementDTO;
-import org.hbrs.se2.project.softwaree.dtos.SkillDTO;
-import org.hbrs.se2.project.softwaree.entities.Job;
-import org.hbrs.se2.project.softwaree.entities.Requirement;
-import org.hbrs.se2.project.softwaree.entities.Skill;
+import org.hbrs.se2.project.softwaree.dtos.*;
+import org.hbrs.se2.project.softwaree.entities.*;
 import org.hbrs.se2.project.softwaree.repository.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JobOfferControlTest {
-
     @Autowired
     JobRepository jobRepository;
     @Autowired
@@ -36,18 +32,20 @@ class JobOfferControlTest {
     @Autowired
     JobOfferControl joc;
 
-    @Test
+   /* @Test
     void createJobOffer() {
 
-    /*    JobDTO jobDTO = new JobDTO(2222, "Test", "test" ,"test");
-        Company company = companyRepository.getOne(3);
-        jobDTO.setCompany(company);
-        joc.createJobOffer(jobDTO, 2222);
-        assertEquals(jobDTO.getTitle() , jobRepository.findJobWithTitleDescriptionLocation(2).getTitle());
-        jobRepository.deleteById(2222);
-*/
+        int size = companyRepository.findCompany(1).getOwJobList().size();
 
-    }
+        JobDTO jobDTO = new JobDTO();
+        jobDTO.setTitle("TestCreateJobOffer");
+        jobDTO.setCompany(companyRepository.getOne(1));
+        joc.createJobOffer(jobDTO,1);
+        assertEquals(companyRepository.findCompany(1).getOwJobList().size() , size+1);
+
+
+
+    }*/
 
     @Test
     void getAvailableSkills() {
@@ -56,8 +54,80 @@ class JobOfferControlTest {
         SkillDTO testSkillDTO = new SkillDTO(testSkill.getId() , testSkill.getDescription() );
         List<SkillDTO> list = joc.getAvailableSkills();
         assertTrue(list.contains(testSkillDTO));
+        assertFalse(list.isEmpty());
+        assertEquals(list.size() , skillRepository.findAll().size());
+    }
+
+    @Test
+    void getAvailableBenefits() {
+
+        List <BenefitDTO> list = joc.getAvailableBenefits();
+        assertFalse(list.isEmpty());
+        assertEquals(list.size() , benefitRepository.findAll().size());
+
 
     }
+
+    @Test
+    void getAvailableRequirements() {
+
+        List <RequirementDTO> list = joc.getAvailableRequirements();
+        assertFalse(list.isEmpty());
+        assertEquals(list.size() , requirementRepository.findAll().size());
+
+    }
+/*
+    @Test
+    void getJobBenefits() {
+
+
+    }
+
+    @Test
+    void getJobRequirements() {
+    }
+
+    @Test
+    void getJobSkills() {
+    }
+*/
+    @Test
+    void createRequirementSet() {
+
+        Set <String> strings = new HashSet<>();
+        strings.add("test1");
+        strings.add("test2");
+        strings.add("test3");
+        Set <Requirement> requirements = joc.createRequirementSet(strings);
+        assertFalse(requirements.isEmpty());
+        assertEquals(3 , requirements.size());
+
+
+
+    }
+
+    @Test
+    void createBenefitSet() {
+        Set <String> strings = new HashSet<>();
+        strings.add("test1");
+        strings.add("test2");
+        strings.add("test3");
+        Set <Benefit> benefits = joc.createBenefitSet(strings);
+        assertFalse(benefits.isEmpty());
+        assertEquals(3 , benefits.size());
+    }
+
+    @Test
+    void createSkillSet() {
+        Set <String> strings = new HashSet<>();
+        strings.add("test1");
+        strings.add("test2");
+        strings.add("test3");
+        Set <Skill> skills = joc.createSkillSet(strings);
+        assertFalse(skills.isEmpty());
+        assertEquals(3 , skills.size());
+    }
+
 
     @Test
     void getJobFromJobID() {
@@ -65,7 +135,6 @@ class JobOfferControlTest {
         assertEquals(testjob.getId() , joc.getJobFromJobID(testjob.getId()).getId());
         assertEquals(testjob.getDescription() , joc.getJobFromJobID(testjob.getId()).getDescription());
         assertEquals(testjob.getTitle() , joc.getJobFromJobID(testjob.getId()).getTitle());
-
     }
 
     @Test
@@ -73,7 +142,6 @@ class JobOfferControlTest {
         RequirementDTO requirementDTO = requirementRepository.getDescription(51);
         assertEquals(requirementDTO.getDescription(), joc.getRequirementFromID(51).getDescription());
         assertEquals(requirementDTO.getId(), joc.getRequirementFromID(51).getId());
-
     }
 
     @Test
